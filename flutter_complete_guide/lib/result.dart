ccb0b73bd59d0c150d1b981b9ac8cd4a97dced36
LokmanHossen <lokmanhossen230@gmail.com>
2024-01-25 01:57:33 +0600

    Calculate Result

---
+++ flutter_complete_guide/lib/result.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final VoidCallback resetHandler;
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'you are awesome and innocent';
+    } else if (resultScore <= 12) {
+      resultText = 'You are ..... strange ?';
+    } else {
+      resultText = 'You are so bad';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          ElevatedButton(
+            onPressed: resetHandler,
+            child: const Text('Restart Quiz !'),
+          ),
+        ],
+      ),
+    );
+  }
+}
