27f70b356997c36ea603380f011a699f4bc35b0d
LokmanHossen <lokmanhossen230@gmail.com>
2024-01-30 11:15:43 +0600

    Personal Expemses_App

---
+++ personal_expenses_app/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import './widgets/user_transaction.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
+  // final List<Transaction> transactions = [
+
+  // ];
+
+  // String? titleInput;
+  // String? amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              child: const Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
+              ),
+            ),
+            UserTransaction(),
+          ],
+        ),
+      ),
+    );
+  }
+}
