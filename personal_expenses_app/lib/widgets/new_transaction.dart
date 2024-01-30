455604bdcce02463d4a26ef09714ecaa4787922c
LokmanHossen <lokmanhossen230@gmail.com>
2024-01-30 13:59:29 +0600

    Change Transaction_Functon

--- personal_expenses_app/lib/widgets/new_transaction.dart
+++ personal_expenses_app/lib/widgets/new_transaction.dart
@@ -5,7 +5,19 @@ class NewTransactionPage extends StatelessWidget {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
-  NewTransactionPage(this.addTx);
+  NewTransactionPage(this.addTx, {super.key});
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +31,7 @@ class NewTransactionPage extends StatelessWidget {
             TextField(
               decoration: const InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => submitData(),
               // onChanged: (value) {
               //   titleInput = value;
               // },
@@ -26,21 +39,22 @@ class NewTransactionPage extends StatelessWidget {
             TextField(
               decoration: const InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
               // onChanged: (value) {
               //   amountInput = value;
               // },
             ),
             TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.purple,
-              ),
+              style: TextButton.styleFrom(foregroundColor: Colors.purple),
+              onPressed: submitData,
               child: const Text('Add Transaction'),
-              onPressed: () {
-                addTx(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
+              // {
+              // addTx(
+              //   titleController.text,
+              //   double.parse(amountController.text),
+              // );
+              // },
             )
           ],
         ),
