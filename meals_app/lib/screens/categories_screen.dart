e5146a90fcf4230dcea76b48af620c26183e9cdd
LokmanHossen <lokmanhossen230@gmail.com>
2024-02-08 02:12:30 +0600

    Categories_Screen

---
+++ meals_app/lib/screens/categories_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../widgets/category_item.dart';
+import 'package:meals_app/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DailyMelas'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: dummyCategories
+            .map((categoryData) => CategoryItem(
+                  categoryData.id,
+                  categoryData.title,
+                  categoryData.color,
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
