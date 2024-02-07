e62644b82fc11a1b196abadac869690a09898adf
LokmanHossen <lokmanhossen230@gmail.com>
2024-02-08 02:09:20 +0600

    MealApp

---
+++ meals_app/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'screens/categories_screen.dart';
+import 'screens/category_meals_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = ThemeData();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MealsApp',
+      theme: ThemeData(
+        primaryColor: Colors.pink,
+        colorScheme: theme.colorScheme.copyWith(
+          secondary: Colors.amber,
+        ),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyLarge: const TextStyle(
+                color: Color.fromRGBO(129, 149, 149, 1),
+              ),
+              bodyMedium: const TextStyle(
+                color: Color.fromRGBO(129, 149, 149, 1),
+              ),
+              titleLarge: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
+      // theme.copyWith(
+      //   colorScheme: theme.colorScheme.copyWith(
+      //     primary: Colors.pink,
+      //     secondary: Colors.amber,
+      //     // secondary: const Color.fromRGBO(225, 254, 229, 1),
+      //   ),
+      // ),
+      // home: const CategoriesScreen(),
+      initialRoute: './',
+      routes: {
+        './': (ctx) => const CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => const CategoryMealsScreen(),
+      },
+    );
+  }
+}
