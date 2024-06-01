60608fc923fbb594ccd78c88dd14d88eb4b0cddd
LokmanHossen <lokmanhossen230@gmail.com>
2024-06-02 02:28:36 +0600

    Update

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -4,6 +4,7 @@ import 'gradient_container.dart';
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: GradientContainer(
           Color.fromARGB(255, 33, 5, 109),
