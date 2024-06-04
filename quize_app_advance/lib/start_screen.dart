c8eaf6fff293ac7686d5d2c42dfc63fdc5cb399a
LokmanHossen <lokmanhossen230@gmail.com>
2024-06-05 01:54:17 +0600

    Update

--- quize_app_advance/lib/start_screen.dart
+++ quize_app_advance/lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuixz, {super.key});
+
+  final void Function() startQuixz;
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +14,15 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
+            color: const Color.fromARGB(120, 255, 255, 255),
           ),
+          //   Opacity(
+          //   opacity: 0.6,
+          //   child:Image.asset(
+          //   'assets/images/quiz-logo.png',
+          //   width: 300,
+          // ),
+          // ),
           const SizedBox(height: 50),
           const Text(
             'Learn Flutter the fun way!',
@@ -27,7 +37,7 @@ class StartScreen extends StatelessWidget {
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
-            onPressed: () {},
+            onPressed: startQuixz,
             icon: const Icon(Icons.arrow_right_alt),
             label: const Text('Start Quize'),
           ),
