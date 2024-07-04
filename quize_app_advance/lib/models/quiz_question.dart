ce65cd2f6d218f2204e9bc26ac8ab67edc3bfff2
LokmanHossen <lokmanhossen230@gmail.com>
2024-07-05 00:51:21 +0600

    Shuffled answerList

--- quize_app_advance/lib/models/quiz_question.dart
+++ quize_app_advance/lib/models/quiz_question.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
 
   final String text;
   final List<String> answer;
+
+  List<String> getShuffeldAnswers() {
+    final shuffledList = List.of(answer);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
